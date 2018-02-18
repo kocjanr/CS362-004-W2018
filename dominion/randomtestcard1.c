4ce25be2aaa1f854ccb3676d0107afa0c331a1f9
Ryan Kojan <rkojan@gmail.com>
2018-02-18 12:42:03 -0800

    smithy random card test

---
+++ dominion/randomtestcard1.c
@@ -0,0 +1,33 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <math.h>
+
+#define DEBUG 0
+#define NOISY_TEST 1
+
+
+int main () {
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+  struct gameState *state = newGame();
+
+  	for(int i =0; i <100000; i++){
+		int x = initializeGame(4,k,1,state);
+        int p = floor(Random() * 2);
+        state->deckCount[p] = floor(Random() * MAX_DECK);
+        state->discardCount[p] = floor(Random() * MAX_DECK);
+        state->handCount[p] = floor(Random() * MAX_HAND);
+
+        for(int j=0; j<5;j++){
+            Smithy(j,state,2);
+        }
+    }
+
+
+  return 0;
+}
